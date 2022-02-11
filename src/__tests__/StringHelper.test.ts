c32a30cef2f8075488b99a1f3e79169229f2b748
farzanhossan <farzanhossans@gmail.com>
2022-02-11 23:59:34 +0600

    Added CamelCaseToSeparateString

--- src/__tests__/StringHelper.test.ts
+++ src/__tests__/StringHelper.test.ts
@@ -1,7 +1,10 @@
-import { IsValidEmail, IsValidPhoneNumber } from '../index';
+import { CamelCaseToSeparateString, IsValidEmail, IsValidPhoneNumber } from '../index';
 test('IsValidPhoneNumber', () => {
     expect(IsValidPhoneNumber('01719020642')).toBe(true);
 });
 test('IsValidEmail', () => {
     expect(IsValidEmail('test@gmail.com')).toBe(true);
+});
+test('CamelCaseToSeparateString', () => {
+    expect(CamelCaseToSeparateString('camelCaseString')).toBe('Camel Case String');
 });
--- src/string.helper.ts
+++ src/string.helper.ts
@@ -9,3 +9,10 @@ export const IsValidEmail = (email: string): boolean => {
 export const IsValidPhoneNumber = (number: string): boolean => {
     return number ? number.match(BD_NUMBER_REGEX) ? true : false : false
 };
+
+//! CamelCaseToSeparateString
+export const CamelCaseToSeparateString = (string: any): string => {
+    const strReplace = string.replace(/([A-Z])/g, ' $1');
+    const str = strReplace.charAt(0).toUpperCase() + strReplace.slice(1);
+    return str;
+};
